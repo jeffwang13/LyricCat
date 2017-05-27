ed63794277ecee5e368fe367f18d63100478a251
Jeffrey Wang <jeffrey868@gmail.com>
2017-05-27 12:15:47 -0400

    WIP

--- scrape.js
+++ scrape.js
@@ -35,6 +35,23 @@ function getLyrics(url) {
                 console.log('Error: ', response.body.error)
             }
         })
+    }, function(err) {
+        let messageData = { text:'Unable to access lyrics.' }
+        request({
+            url: 'https://graph.facebook.com/v2.6/me/messages',
+            qs: {access_token:process.env.PAGE_ACCESS_TOKEN},
+            method: 'POST',
+            json: {
+                recipient: {id:sender},
+                message: messageData,
+            }
+        }, function(error, response, body) {
+            if (error) {
+                console.log('Error sending messages: ', error)
+            } else if (response.body.error) {
+                console.log('Error: ', response.body.error)
+            }
+        })
     });
 }
 
