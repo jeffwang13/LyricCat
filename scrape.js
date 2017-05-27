39432d1342692a3aecdb55e1e5afb5274af82ae1
Jeffrey Wang <jeffrey868@gmail.com>
2017-05-27 00:51:18 -0400

    Revert "WIP"
    This reverts commit 8eb3cefcfde34adc5c81ccb6b96e28122d6e0895.

--- scrape.js
+++ scrape.js
@@ -2,19 +2,17 @@ const request = require('request');
 var xpath = require('xpath')
     , dom = require('xmldom').DOMParser
 
-// console.log(getLyrics('http://www.azlyrics.com/lyrics/desiigner/panda.html'))
-//
-// function getLyrics (url){
+function getLyrics (url){
   request({
-      uri: 'http://www.azlyrics.com/lyrics/desiigner/panda.html',
+      uri: url,
   }, function(error, response, body) {
       const doc = new dom().parseFromString(body);
       const lyrics = xpath.select("/html/body/div[@class='container main-page']/div[@class='row']/div[@class='col-xs-12 col-lg-8 text-center']/div[5]", doc).toString();
-      console.log(lyrics);
+      return lyrics;
   });
-//   return '';
-// }
+  return '';
+}
 
-//module.exports.getLyrics = getLyrics;
+module.exports.getLyrics = getLyrics;
 
 
