db832a89afdbdef0ecc0b710157efe81aee2ac80
Jeffrey Wang <jeffrey868@gmail.com>
2020-06-12 12:34:04 -0400

    refactor youtubeapi

--- api/youtubeApi.js
+++ api/youtubeApi.js
@@ -2,32 +2,32 @@ const mailer = require('../lib/mailer')
 const youtubeSearch = require( 'yt-search' )
 
 function getDanceCover(song, artist, sender, callback) {
-    const opts = {
-        query: `${song} ${artist} dance cover`,
-        pageStart: 1,
-        pageEnd: 1
-    }
-    youtubeSearch( opts, function ( err, r ) {
-        if (err || typeof(r.videos[0]) === 'undefined') {
-            let message = `Oh no! There was a problem with the official YouTube API, please try again.`
-            mailer.sendTextMessage(sender, message)
-        } else {
-            const topResult = r.videos[0]
-            callback({title: topResult.title, url: topResult.url, image: topResult.image})
-        }
+    query = `${song} ${artist} dance cover`
+    executeSearch(query, sender, function(result) {
+        callback(result)
     })
 }
 
 function getGuitarTutorial(song, artist, sender, callback) {
+    query = `${song} ${artist} guitar tutorial`
+    executeSearch(query, sender, function(result) {
+        callback(result)
+    })
+}
+
+function executeSearch(query, sender, callback) {
     const opts = {
-        query: `${song} ${artist} guitar tutorial`,
+        query: query,
         pageStart: 1,
         pageEnd: 1
     }
-    youtubeSearch( opts, function ( err, r ) {
+    youtubeSearch( opts, function (err, r) {
         if (err || typeof(r.videos[0]) === 'undefined') {
-            let message = `Oh no! There was a problem with the official YouTube API, please try again.`
-            mailer.sendTextMessage(sender, message)
+            setTimeout(function(){
+                executeSearch(query, sender, function(result) {
+                    callback(result)
+                })
+            }, 1000)
         } else {
             const topResult = r.videos[0]
             callback({title: topResult.title, url: topResult.url, image: topResult.image})
